cd2d40b2b288643184f6cb0f282f821d03d0d61b
Md. Mamunur Rashid <39217824+Mamun-tech@users.noreply.github.com>
2018-05-28 17:44:08 +0600

    Add files via upload

---
+++ Bluetooth controlled led.ino
@@ -0,0 +1,37 @@
+int LED = 4;
+String readString;
+void setup() {
+    pinMode (LED, OUTPUT);
+
+    Serial.begin(9600);
+}
+void loop() {
+  while(Serial.available()){  //Check if there is an available byte to read 
+    delay(50);                //Delay added to make thing stable 
+    char c = Serial.read();   //Conduct a serial read 
+    readString+=c;
+  }
+
+  if(readString.length() > 0) {
+    Serial.println(readString);
+    if(readString == "ON"){
+       LED_on();
+    }
+
+    if(readString == "OFF"){
+       LED_off();
+    }
+    
+
+    readString = "";
+  }
+}
+void LED_on ()
+{
+   digitalWrite(LED, HIGH);
+}
+void LED_off ()
+{
+   digitalWrite(LED, LOW);
+}
+
